f0e60179e57cd8da5411690e54eae81d2e02d417
Vijay Pabial <vijaypabial@yahoo.com>
2020-05-12 21:14:37 +1000

    Created seeds file and created some data

--- db/migrate/20200512074439_drop_wines.rb
+++
@@ -1,5 +0,0 @@
-class DropWines < ActiveRecord::Migration[6.0]
-  def change
-  	drop_table :wines
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+9.times do |vineyard|
+	Vineyard.create!(
+		title: "Mudhouse #{vineyard}",
+		about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Why do we use it?
+
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+",
+		main_image: "https://via.placeholder.com/500x300"
+		)
+end
+
+12.times do |wine|
+	Wine.create!(
+	title: "Mud House#{wine}",
+	variety: "Sauvignon Blanc #{wine}",
+	description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse",
+	image: "https://via.placeholder.com/300x150"
+	)
+end
