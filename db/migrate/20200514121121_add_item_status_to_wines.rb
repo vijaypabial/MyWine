ddd248623ef15b9cc744ffaa04d53e833582c546
Vijay Pabial <vijaypabial@yahoo.com>
2020-05-14 22:15:43 +1000

    sorted migration files and enum

--- db/migrate/20200514121121_add_item_status_to_wines.rb
+++ db/migrate/20200514121121_add_item_status_to_wines.rb
@@ -1,5 +1,5 @@
 class AddItemStatusToWines < ActiveRecord::Migration[6.0]
   def change
-    add_column :wines, :status, :integer
+    add_column :wines, :status, :integer, default: 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_112516) do
+ActiveRecord::Schema.define(version: 2020_05_14_121121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,12 +64,9 @@ ActiveRecord::Schema.define(version: 2020_05_14_112516) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
-    t.bigint "vineyard_id", null: false
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_wines_on_slug", unique: true
-    t.index ["vineyard_id"], name: "index_wines_on_vineyard_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "wines", "vineyards"
 end
