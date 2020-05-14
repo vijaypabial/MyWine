29aa4aa7262a851f3c18bd0f6737c76339380ddc
Vijay Pabial <vijaypabial@yahoo.com>
2020-05-14 15:08:47 +1000

    added enum and publish function

--- db/migrate/20200514033140_add_item_status_to_wines.rb
+++
@@ -1,5 +0,0 @@
-class AddItemStatusToWines < ActiveRecord::Migration[6.0]
-  def change
-    add_column :wines, :status, :integer, default: 0
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ Why do we use it?
 
 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 ",
-		main_image: "https://via.placeholder.com/500x300"
+		main_image: "image_tag ("download.jpeg")"
 		)
 end
 puts "Hello"
@@ -15,7 +15,7 @@ puts "Hello"
 	title: "Mud House#{wine}",
 	variety: "Sauvignon Blanc #{wine}",
 	description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse",
-	wine_image: "https://via.placeholder.com/300x150",
+	wine_image: "image_tag ("download.jpeg")"
 	price: "15.99"
 	)
 end
