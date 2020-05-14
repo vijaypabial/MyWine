91280e4a08a95418b634787fb3101cb698cdbc54
Vijay Pabial <vijaypabial@yahoo.com>
2020-05-15 04:30:16 +1000

    Added realtions in models

--- app/models/vineyard.rb
+++ app/models/vineyard.rb
@@ -1,3 +1,4 @@
 class Vineyard < ApplicationRecord
+    has_many :wines
     validates_presence_of :title, :about, :main_image
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,5 @@
 class Wine < ApplicationRecord
+  belongs_to :vineyard
   enum status: {draft: 0, published: 1}
     extend FriendlyId
   friendly_id :title, use: :slugged
