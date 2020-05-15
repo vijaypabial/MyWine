18c42fa67802a2b7545c11184de90446d3fa4e8a
Vijay Pabial <vijaypabial@yahoo.com>
2020-05-15 15:57:32 +1000

    image set

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,11 @@
 		main_image: "https://via.placeholder.com/300x150"
 	)
 end
-puts "Hello"
 12.times do |wine|
 	Wine.create!(
 	vineyard_id: Vineyard.last.id,
 	title: "Mud House#{wine}",
-	wine_image: "https://via.placeholder.com/300x150",
+	wine_image: "https://via.placeholder.com/300x150", ##this is supposed to be active storage
 	variety: "Sauvignon Blanc #{wine}",
 	description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse",
 	price: "15.99",
