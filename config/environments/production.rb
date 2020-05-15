8590c3f55a9745816d3e2fe2d1c438427628557c
Vijay Pabial <vijaypabial@yahoo.com>
2020-05-15 15:23:31 +1000

    image displacement

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.serve_static_assets = true
+  #config.serve_static_assets = true
 
 
   # Full error reports are disabled and caching is turned on.
