61069a316396d3c632a31394f12993f89ff00e79
Vijay Pabial <vijaypabial@yahoo.com>
2020-05-16 15:57:09 +1000

    Added cloudinary and its tested and working

--- Gemfile
+++ Gemfile
@@ -56,3 +56,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'friendly_id', '~> 5.2.4'
 
 gem 'devise'
+
+gem 'cloudinary'
+
+gem 'activestorage-cloudinary-service'
--- app/models/vineyard.rb
+++ app/models/vineyard.rb
@@ -1,7 +1,8 @@
 class Vineyard < ApplicationRecord
-    has_many :wines, dependent: :destroy
 
     has_one_attached :main_image
-    
+
+    has_many :wines, dependent: :destroy
+
     validates_presence_of :title, :about, :main_image
 end
