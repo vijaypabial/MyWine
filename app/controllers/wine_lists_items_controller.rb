ea401f21d90d37a42dc204f6920b0199c98f8463
Vijay Pabial <vijaypabial@yahoo.com>
2020-05-19 14:39:55 +1000

    Sorted views

--- app/controllers/wine_lists_items_controller.rb
+++ app/controllers/wine_lists_items_controller.rb
@@ -1,8 +1,23 @@
 class WineListsItemsController < ApplicationController
+before_action :wine, only: [:index, :create]
+    def create
+       if [@wines.ids].each.include? "params[:wine_id]"
+         puts "This item exists already"
+       else
+         current_user.wine_lists_items.create!(wine_id: params[:wine_id])
+         redirect_to
+       end
+    end
 
     def index
         @wines = current_user.wines
     end
 
+    private
 
+    def wine
+        @wines = current_user.wines
+    end
+        
+       
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 Rails.application.routes.draw do
-  
+  resources :vineyards
+  resources :orders, only: [:new]
+  devise_for :users, path: "", path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   
   get 'orders/new'
   get 'orders/cancel', to:'pages#home'
   get 'orders/complete', to: 'orders#complete'
-  devise_for :users, path: "", path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  get "my_wines", to: "wine_lists_items#index", as: "my_wines"
+  
+  post "my_wines", to: "wine_lists_items#create", as: "my_wines"
+  get "my_wines", to: "wine_lists_items#index"
   get "our_wines", to: "vineyards#our_wines", as: "our_wines"
-  resources :orders, only: [:new]
-  resources :vineyards
+ 
   get "vineyards", to: "vineyards#index"
   get "wines", to: "wines#index"
-  get "my_cart", to:"pages#my_cart"
   get "contact", to:"pages#contact"
-  
-  
 
   resources :wines do
     member do
-      get :toggle_status
+    get :toggle_status
     end
   end
  
